27e79b72fc30cf68174fae8764e149ddc6ff7386
Sergey Baranov <thx4win@gmail.com>
2021-09-20 01:13:18 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    comission("Mastercard", 7000_00.0, 5000_00.0)
+}
+
+fun comission(typeCard: String = "Vk Pay", sumTransfer: Double = 0.0, currentTransfer: Double = 0.0) {
+    var sumComission = 0.0
+    var odds = 75000_00.0 - sumTransfer
+    when (typeCard) {
+        "Mastercard", "Maestro" -> sumComission = if (odds >= currentTransfer) 0.0 else currentTransfer * 0.006 + 20_00
+        "Visa", "Мир" -> sumComission = if (currentTransfer * 0.0075 < 35_00) 3500.0 else currentTransfer * 0.0075
+        "Vk Pay" -> sumComission = 0.0
+    }
+    println("$sumComission")
+}
