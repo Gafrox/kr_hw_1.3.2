9a53ec6f403de679c4b4aade6cbf892ec78267be
Sergey Baranov <thx4win@gmail.com>
2021-09-29 02:09:42 +0300

    Add Test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,14 @@
 fun main() {
-    comission("Mastercard", 7000_00.0, 5000_00.0)
+    println(comission("Mastercard", 7000_00.0, 5000_00.0))
 }
 
-fun comission(typeCard: String = "Vk Pay", sumTransfer: Double = 0.0, currentTransfer: Double = 0.0) {
+fun comission(typeCard: String = "Vk Pay", sumTransfer: Double = 0.0, currentTransfer: Double = 0.0): Double {
     var sumComission = 0.0
-    var odds = 75000_00.0 - sumTransfer
+    val odds = 75000_00.0 - sumTransfer
     when (typeCard) {
         "Mastercard", "Maestro" -> sumComission = if (odds >= currentTransfer) 0.0 else currentTransfer * 0.006 + 20_00
         "Visa", "Мир" -> sumComission = if (currentTransfer * 0.0075 < 35_00) 3500.0 else currentTransfer * 0.0075
         "Vk Pay" -> sumComission = 0.0
     }
-    println("$sumComission")
+    return sumComission
 }
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,81 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun comission_forMasterCardAndMaestro() {
+        val typeCard = "Mastercard"
+        val sumTransfer = 50000_00.0
+        val currentTransfer = 10000_00.0
+
+        val result = comission(
+            typeCard = typeCard,
+            sumTransfer = sumTransfer,
+            currentTransfer = currentTransfer
+        )
+
+        assertEquals(0.0, result, 0.0)
+    }
+
+    @Test
+    fun comission_forMasterCardAndMaestro_underLimit() {
+        val typeCard = "Mastercard"
+        val sumTransfer = 75000_00.0
+        val currentTransfer = 10000_00.0
+
+        val result = comission(
+            typeCard = typeCard,
+            sumTransfer = sumTransfer,
+            currentTransfer = currentTransfer
+        )
+
+        assertEquals(80_00.0, result, 0.0)
+    }
+
+    @Test
+    fun comission_forVisaAndMir() {
+        val typeCard = "Visa"
+        val sumTransfer = 0.0
+        val currentTransfer = 500_00.0
+
+        val result = comission(
+            typeCard = typeCard,
+            sumTransfer = sumTransfer,
+            currentTransfer = currentTransfer
+        )
+
+        assertEquals(35_00.0, result, 0.0)
+    }
+
+    @Test
+    fun comission_forVisaAndMir_underLimit() {
+        val typeCard = "Visa"
+        val sumTransfer = 0.0
+        val currentTransfer = 5000_00.0
+
+        val result = comission(
+            typeCard = typeCard,
+            sumTransfer = sumTransfer,
+            currentTransfer = currentTransfer
+        )
+
+        assertEquals(37_50.0, result, 0.0)
+    }
+
+    @Test
+    fun comission_forVkPay() {
+        val typeCard = "Vk Pay"
+        val sumTransfer = 900000_00.0
+        val currentTransfer = 5000_00.0
+
+        val result = comission(
+            typeCard = typeCard,
+            sumTransfer = sumTransfer,
+            currentTransfer = currentTransfer
+        )
+
+        assertEquals(0.0, result, 0.0)
+    }
+}
